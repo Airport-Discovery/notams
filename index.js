6cc1367f88eed9b7bf9852147da363986ce49a22
Hatem Alimam <me@hatemalimam.com>
2020-12-14 17:05:00 +0300

    Support for array of ICAO + README

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const axios = require('axios');
 
 const SEARCH_URL = 'https://notams.aim.faa.gov/notamSearch/search';
 
-const getNotams = async icao => {
-  const data = `searchType=0&designatorsForLocation=${icao}`;
+const getNotams = async icaos => {
+  if (Array.isArray(icaos)) {
+    icaos = icaos.join(',');
+  }
+
+  const data = `searchType=0&designatorsForLocation=${icaos}`;
   const response = await axios.post(SEARCH_URL, data);
 
   if (response && response.data) {
